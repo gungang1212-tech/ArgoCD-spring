cba283feb2eeb971e3d8541c3920adaa838dd38c
gungang1212-tech <gungang1212@gmail.com>
2026-04-08 15:19:56 +0900

    0319

--- src/main/java/kr/co/gitops/HomeController.java
+++ src/main/java/kr/co/gitops/HomeController.java
@@ -3,7 +3,7 @@ package kr.co.gitops;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//SSsss
+//SSssssss
 @RestController
 public class HomeController {
 	 
